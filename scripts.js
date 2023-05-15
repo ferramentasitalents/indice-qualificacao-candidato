7c9c2b493f8074341a84276a797d8afa5cc67b6d
ferramentasitalents <133061937+ferramentasitalents@users.noreply.github.com>
2023-05-15 17:10:56 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
+function calculateQualificationIndex() {
+  const numCandidatosQualificados = parseFloat(document.getElementById("num-candidatos-qualificados").value);
+  const numCandidatosScreening = parseFloat(document.getElementById("num-candidatos-screening").value);
+  const qualificationIndex = (numCandidatosQualificados / numCandidatosScreening) * 100;
+  document.getElementById("result").innerHTML = `Índice de Qualificação de Candidatos: ${qualificationIndex.toFixed(2)}%`;
+}
 
